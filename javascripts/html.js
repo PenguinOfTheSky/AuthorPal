d9e39ac36f752038000b990a5889f17c890e4206
T A Russell <Dune-planet@hotmail.com>
2017-08-26 22:51:30 -0400

    minor changes

--- javascripts/html.js
+++ javascripts/html.js
@@ -3,11 +3,17 @@ Book.html = {
     start: function() {
       let box = document.createElement('div')
       Object.assign(box, {
-        style: Book.css.glass + 'box-sizing:border-box;',
+        style: Book.css.glass,
         id: 'Book.html.display'
       })
-      box.style = Book.css.glass;
       let root = box.createShadowRoot()
+      let style = document.createElement('style');
+      style.innerText = `
+      h4 {
+        text-align:center;
+      }
+      `
+      root.appendChild(style)
       let startMessage = document.createElement('div')
       startMessage.innerHTML = `<h4>To get started click the buttons above ^ </h4>
         Lycelia's <i>AuthorPal</i>
