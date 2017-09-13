2aa2e56921d351e8dc63c9e10aae4ceefa9067ec
T A Russell <Dune-planet@hotmail.com>
2017-09-13 16:17:38 -0400

    css theme fix

--- javascripts/css.js
+++ javascripts/css.js
@@ -6,12 +6,11 @@ if (theme == undefined) {
   theme = 'default'
 }
 let _ = Book.cssTemplates[theme];
-console.log(typeof(_))
 if (typeof(_) == 'function') _ = _()
 Book.events.updatePreferences = function(newTheme) {
   localforage.setItem('Book', JSON.stringify(Book.data.local), function(err){})
   _ = Book.cssTemplates[newTheme];
-  if (typeof(_) == 'function') _ = theme()
+  if (typeof(_) == 'function') _ = _()
 }
 return {
   modal: `
